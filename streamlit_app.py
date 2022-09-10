38a90e41a1d63bf8133cf880fa20a635d301ad56
mpstpierrehome <77597693+mpstpierrehome@users.noreply.github.com>
2022-09-10 19:33:53 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,8 +3,6 @@ import streamlit
 streamlit.title('My parents new healthy dinner')
 streamlit.header('Breakfast menu')
 
-🥣  🐔 🍞
-
 streamlit.text('🥗 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥑 Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
